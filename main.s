fcfe5c99662c055a3a354aa6a4449b363665a0e5
pdw19 <pdw19@skbl-422-014.ph.ic.ac.uk>
2022-03-17 14:03:23 +0000

    Wrote code for timer interrupts

--- main.s
+++ main.s
@@ -1,23 +1,37 @@
 #include <xc.inc>
 
-extrn	touchscreen_setup, touchscreen_detect
+extrn	touchscreen_setup, touchscreen_detect, timer_setup, timer_on, timer_int_hi
 
-psect	code, abs
+;psect	code, abs
 
-org	    100h
+;org	    100h
 
-setup:
-    call    touchscreen_setup
-    clrf    PORTH, A
-    clrf    TRISH, A
-    clrf    PORTJ, A 
-    clrf    TRISJ, A
+;setup:
+;    call    touchscreen_setup
+;    clrf    PORTH, A
+;    clrf    TRISH, A
+;    clrf    PORTJ, A 
+;    clrf    TRISJ, A
     
-    movlw   0x00
+;    movlw   0x00
 
+
+;main:
+;    call    touchscreen_detect
+;    bra	    main
+
+;    end
+
+interrupt:
+    org	    0x0008
+    call    timer_int_hi
 
 main:
-    call    touchscreen_detect
-    bra	    main
-
+    org	    100h
+    clrf    TRISD, A
+    clrf    LATD, A
+    
+    call    timer_setup
+    call    timer_on
+    
     end
---
+++ timer.s
@@ -0,0 +1,92 @@
+#include <xc.inc>
+
+global	timer_setup, timer_on, timer_int_hi
+
+
+psect	udata_acs
+
+counter:    ds	1
+
+
+psect	code, abs
+
+org		200h
+
+timer_setup:
+    bsf		TMR0IE			; Interrupts to be triggered by timer 0
+    bsf		GIE			; Enable all interrupts
+    
+    movlw	00000111B		; Set timer 0 to 16-bit, Fosc/4/256
+    movwf	T0CON, A		; = 62.5 kHz clock rate, ~ 1 s rollover
+    
+    return
+
+
+timer_on:
+    movlw	0x0A			; Set counter to 10
+    movwf	counter, A
+    
+    ;call	show 10 on counter
+    movlw	0x0A
+    movwf	PORTD, A
+    
+    bsf		TMR0ON, A		; Turn on timer 0
+    
+    return
+
+
+timer_int_hi:
+    btfss	TMR0IF			; Check this is timer 0 interrupt
+    retfie	f			; Return if not
+    
+    decf	counter, A		; decrement counter
+    
+test_for_9:
+    movlw	0x09			; Compare counter to 9
+    CPFSEQ	counter, A		; Skip if equal to 9
+    bra		test_for_8		; Go to test if counter is 8
+    ;call	show 9 on display	; Show 9 on display
+    movlw	0x09
+    movwf	PORTD, A
+    goto	timer_int_hi_end	; Go to function return
+    
+test_for_8:
+    movlw	0x08			; Compare counter to 8
+    CPFSEQ	counter, A		; Skip if equal to 8
+    bra		test_for_7		; Go to test if counter is 7
+    ;call	show 8 on display	; Show 8 on display
+    movlw	0x08
+    movwf	PORTD, A
+    goto	timer_int_hi_end	; Go to function return
+
+test_for_7:
+    movlw	0x07			; Compare counter to 7
+    CPFSEQ	counter, A		; Skip if equal to 7
+    bra		test_for_6		; Go to test if counter is 0
+    ;call	show 7 on display	; Show 7 on display
+    movlw	0x07
+    movwf	PORTD, A
+    goto	timer_int_hi_end			; Go to function return
+
+test_for_0:
+    movlw	0x00			; Compare counter to 0
+    CPFSEQ	counter, A		; Skip if equal to 0
+    bra		question_mark		; Go to show ? if counter is 6-1
+    ;call	show ? on display	; Show ? on display
+    movlw	0xFF
+    movwf	PORTD, A
+    bcf		TMR0ON, A		; Turn off timer 0
+    goto	timer_int_hi_end	; Go to function return
+
+question_mark:
+    ;call	show ? on display	; Show ? on display
+    movlw	0xFF
+    movwf	PORTD, A
+    goto	timer_int_hi_end	; Go to function return
+
+timer_int_hi_end:
+    bcf		TMR0IF			; Clear interrupt flag
+    retfie	f
+
+
+end
